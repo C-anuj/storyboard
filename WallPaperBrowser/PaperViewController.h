063e2f883236eb5e7f2e15320b1bc884d87c2f84
C-anuj <anuj@pocketgems.com>
2014-08-22 11:54:44 -0700

    storyboard example

---
+++ WallPaperBrowser/PaperViewController.h
@@ -0,0 +1,15 @@
+//
+//  PaperViewController.h
+//  WallPaperBrowser
+//
+//  Created by anuj on 8/22/14.
+//  Copyright (c) 2014 anuj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaperViewController : UIViewController
+
+@property (nonatomic, assign)int monthToShow;
+
+@end
--- WallPaperBrowser/ViewController.m
+++ WallPaperBrowser/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "PaperViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *sliderLabel;
+@property (weak, nonatomic) IBOutlet UISlider *sliderValue;
 
 @end
 
@@ -20,10 +23,30 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)gotapper:(id)sender {
+    [self performSegueWithIdentifier:@"GoToPaper" sender:self];
+}
+
+- (IBAction)close:(UIStoryboardSegue*)segue {
+    
+}
+
+- (IBAction)sliderValueChanged:(id)sender {
+    self.sliderLabel.text = [NSString stringWithFormat:@"%d", (int)self.sliderValue.value];
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"GoToPaper"]) {
+        PaperViewController *paperViewController = (PaperViewController *)segue.destinationViewController;
+        paperViewController.monthToShow = self.sliderLabel.text.intValue;
+    }
 }
 
 @end
